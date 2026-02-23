dd5dae54802b913c19c86cadc6b7fa5c30d47dd0
ashleyirispuppy <ashley@ggtylerr.dev>
2026-02-23 16:39:20 +0100

    Update css/stats.js

--- css/stats.js
+++ css/stats.js
@@ -22,7 +22,7 @@
   } catch (e) {
     }
 
-   const url = "/static/improving-poke.js?v=4";
+   const url = "/static/improving-poke.js?v=45";
   if (document.querySelector(`script[src="${url}"]`)) return;
 
   const s = document.createElement("script");
